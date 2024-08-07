e6734554e55559044de98a7e41d5bcc0b9880790
ibrahimmesan <122969970+ibrahimmesan@users.noreply.github.com>
2024-08-07 21:39:31 +0200

    revert

--- app/(tabs)/community.tsx
+++ app/(tabs)/community.tsx
@@ -1,46 +1,12 @@
 import ParallaxScrollView from "@/components/animated/ParallaxScrollView";
 import { ThemedView } from "@/components/themed/ThemedView";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import { StyleSheet, Switch } from "react-native";
-import {
-  AuthorizationPermissions,
-  FitnessDataType,
-  FitnessTracker,
-  GoogleFitDataType,
-  HealthKitDataType,
-} from "@kilohealth/rn-fitness-tracker";
-import { useEffect, useState } from "react";
+import { StyleSheet } from "react-native";
+import { useState } from "react";
 import TextDripsy from "@/components/themed/TextDripsy";
-
-const permissions: AuthorizationPermissions = {
-  healthReadPermissions: [HealthKitDataType.StepCount],
-  googleFitReadPermissions: [GoogleFitDataType.Steps],
-};
 
 export default function CommunityScreen() {
   const [steps, setSteps] = useState(0);
-
-  const getStepsToday = async () => {
-    try {
-      const authorized = await FitnessTracker.authorize(permissions);
-
-      if (!authorized) return;
-
-      const stepsToday = await FitnessTracker.getStatisticTodayTotal(
-        FitnessDataType.Steps
-      );
-      setSteps(stepsToday);
-      // returns the number of steps walked today, e.g. 320
-      console.debug("res: ", stepsToday);
-    } catch (error) {
-      // Handle error here
-      console.debug("err:", error);
-    }
-  };
-
-  useEffect(() => {
-    getStepsToday();
-  }, []);
 
   return (
     <ParallaxScrollView
